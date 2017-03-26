748949bf2cd6d3bc14d4b11f5a1dc4d94c17c21c
Ellen Sun <heeycutie@yahoo.com>
2017-03-26 01:36:13 -0400

    testing out sections of html

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-// external js: masonry.pkgd.js, imagesloaded.pkgd.js
+$(document).ready(function() {
+  // external js: masonry.pkgd.js, imagesloaded.pkgd.js
 
-var grid = document.querySelector('.grid');
-var msnry;
+  var grid = document.querySelector('.grid');
+  var msnry;
 
-imagesLoaded( grid, function() {
-  // init Isotope after all images have loaded
-  msnry = new Masonry( grid, {
-    itemSelector: '.grid-item',
-    columnWidth: '.grid-sizer',
-    percentPosition: true,
-    gutter: 10
+  imagesLoaded( grid, function() {
+    // init Isotope after all images have loaded
+    msnry = new Masonry( grid, {
+      itemSelector: '.grid-item',
+      columnWidth: '.grid-sizer',
+      percentPosition: true,
+      gutter: 10
+    });
   });
+  $('#section').load('portfolio.html');
+
 });
